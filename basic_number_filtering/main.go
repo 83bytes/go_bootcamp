2d4e3e06729edbe1ae08e6e0e22d9b0bcda00556
Sohom Bhattacharjee <soham.bhattacharjee15@gmail.com>
2023-11-11 20:06:30 +0530

    basic IsEven is running. with tests

--- basic_number_filtering/main.go
+++ basic_number_filtering/main.go
@@ -9,3 +9,10 @@ func Hello() string {
 func main() {
 	fmt.Println(Hello())
 }
+
+func IsEven(x int) bool {
+	res := float64(x) / 2
+	// we divide the number by 2.
+	// if the quotient is a fraction, then the number is not an integer
+	return res == float64(int(res))
+}
--- basic_number_filtering/main_test.go
+++ basic_number_filtering/main_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestHello(t *testing.T) {
 	got := Hello()
@@ -8,5 +10,30 @@ func TestHello(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func TestIsEven(t *testing.T) {
+	type args struct {
+		x int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{"Two is even!", args{2}, true},
+		{"Negetive Two is even!", args{-2}, true},
+		{"Zero is even!", args{0}, true},
+		{"One is not even!", args{1}, false},
+		{"Negetive One is not even!", args{1}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsEven(tt.args.x); got != tt.want {
+				t.Errorf("IsEven() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
